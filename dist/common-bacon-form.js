ed21d6cce79724755cc544d63398470962b9f711
Atte Keinänen <atte.keinanen@gmail.com>
2014-05-28 15:55:32 +0300

    Added integer, date, empty string & anything validators.

--- dist/common-bacon-form.js
+++ dist/common-bacon-form.js
@@ -206,11 +206,15 @@
         'PASSWORD' : function isValidPassword(pwd) { return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,50}$/.test(pwd) },
         'NOT_EMPTY' : function notEmpty(val) { return val !== "" },
         'IS_EMPTY' : function isEmpty(field) { return field.map(function(value) { return value === "" }) },
+        'IS_EMPTY_STRING' : function isEmpty(val) { return val === "" },
         'AUTH_CODE': function authCode(val) { return val.length === 5 },
         'MOBILE_WITH_PREFIX' : function containsCountryCode(val) { return /^(?:00|\+)[0-9\s]{6,20}$/.test(val) },
         'MOBILE' : function containsCountryCode(val) { return /^[0-9\s]{6,20}$/.test(val) },
         'IS_TRUE' : function isTrue(bool) { return bool === true },
-        'IS_FALSE' : function isFalse(bool) { return false }
+        'IS_FALSE' : function isFalse(bool) { return false },
+        'IS_INTEGER' : function isNumber(val) { return val >>> 0 === parseFloat(val) },
+        'IS_DATE': function isDate(val) { return /^\d{1,2}[./-]\d{1,2}[./-]\d{4}$/.test(val)},
+        'ANYTHING': function alwaysTrue() { return true }
       }
     }
 
